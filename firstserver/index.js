50b171a76161a9ef83c38b445dc319f1b0f0cfc7
Shrutarshi Ray <91513676+shrutarshi2002@users.noreply.github.com>
2023-08-04 10:14:50 +0530

    add me

---
+++ firstserver/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/', function(req, res){
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send('The result of the calculation is ' + result);
+})
+
+
+app.listen(4000, function(){
+    console.log("Running on port 4000...");
+} )
